305a61258dc0f0d71e3e329ce9583b44f439945b
Lukas Jelinek <lukx19@gmail.com>
2018-07-16 15:17:27 +0200

    Custome.js change of CRLF to LF

--- delcomp/public/js/customer.js
+++ delcomp/public/js/customer.js
@@ -1,4 +1,4 @@
 frappe.provide("erpnext.utils");
 
 // cleaning up customer billed balance from dashboard
-erpnext.utils.set_party_dashboard_indicators = function (frm) {};
+erpnext.utils.set_party_dashboard_indicators = function (frm) { };
