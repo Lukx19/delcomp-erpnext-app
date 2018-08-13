77a3eac22dd37fe4f31ed9b90052baf29e1f949c
Lukas Jelinek <lukx19@gmail.com>
2018-08-13 15:42:59 +0200

    Show desk icon on mobile. Remove scrollbar from calendar. Fix loading err 500 on mobile app.

--- delcomp/hooks.py
+++ delcomp/hooks.py
@@ -15,8 +15,8 @@ app_license = "MIT"
 # ------------------
 
 # include js, css files in header of desk.html
-app_include_css = "/assets/css/delcomp.min.css"
-app_include_js = ["/assets/js/delcomp.min.js"]
+app_include_css = "assets/css/delcomp.min.css"
+app_include_js = ["assets/js/delcomp.min.js"]
 
 # include js, css files in header of web template
 # web_include_css = "/assets/delcomp/css/delcomp.css"
