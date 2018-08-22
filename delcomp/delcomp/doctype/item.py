e779ae05e227675932d70c8703a7fa262293724c
Lukas Jelinek <lukx19@gmail.com>
2018-08-22 06:19:24 +0200

    Adding support for variants and diffrent stock uoms in UOM conversion generation

--- delcomp/delcomp/doctype/item.py
+++ delcomp/delcomp/doctype/item.py
@@ -20,11 +20,29 @@ def set_uom_value(doc, uom, val):
 
 def gen_UOM(doc, method):
     frappe.errprint("gen")
+    # set length value if variant is used
+    if doc.variant_attribute:
+        for row in doc.attributes:
+            if row.attribute == doc.variant_attribute and row.attribute_value:
+                doc.length = int(row.attribute_value)
+                break
+
     if not (doc.length and doc.width and doc.height):
         return
-    m3 = doc.length * doc.width * doc.height / 1000000000
-    m2 = doc.length * doc.width / 1000000
-    set_uom_value(doc, "m3", m3)
-    set_uom_value(doc, "m2", m2)
-    set_uom_value(doc,"bm",doc.length/1000)
-    doc.weight_per_unit = m3 * doc.density
+
+    if doc.stock_uom == "ks":
+        m3 = doc.length * doc.width * doc.height / 1000000000
+        m2 = doc.length * doc.width / 1000000
+        set_uom_value(doc, "bm", doc.length / 1000)
+        set_uom_value(doc, "m3", m3)
+        set_uom_value(doc, "m2", m2)
+        doc.weight_per_unit = m3 * doc.density
+    if doc.stock_uom == "bm":
+        m3 = 1000 * doc.width * doc.height / 1000000000
+        m2 = 1000 * doc.width / 1000000
+        set_uom_value(doc, "ks", doc.length / 1000)
+        set_uom_value(doc, "m3", m3)
+        set_uom_value(doc, "m2", m2)
+        doc.weight_per_unit = m3 * doc.density
+
+
