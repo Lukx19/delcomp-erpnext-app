97cfc67b0f54a57d3793aa66509e3fb281fa4548
Lukas Jelinek <lukx19@gmail.com>
2018-08-31 19:51:48 +0200

    Customizing delivery note and purchase receipt

--- delcomp/public/js/delivery_note.js
+++ delcomp/public/js/delivery_note.js
@@ -1,6 +1,14 @@
 frappe.ui.form.on("Delivery Note", {
+    setup: function () {
+        console.log("setup")
+        frappe.call({
+            method: "delcomp.delcomp.doctype.stock.overridejs_get_item_details",
+            args: {},
+            callback: function (r) { }
+        });
+    },
     onload: function () {
-        // console.log("onload")
+        console.log("onload")
         frappe.call({
             method: "delcomp.delcomp.doctype.stock.overridejs_get_item_details",
             args: {},
@@ -8,11 +16,30 @@ frappe.ui.form.on("Delivery Note", {
         });
     },
     resfresh: function () {
-        // console.log("onload")
+        console.log("refresh")
         frappe.call({
             method: "delcomp.delcomp.doctype.stock.overridejs_get_item_details",
             args: {},
             callback: function (r) {}
         });
+    },
+
+    onload_post_render: function (frm) {
+        var grid = frm.get_field("items").grid
+        grid.multiple_set = false
+        var btn = $(grid.wrapper).find(".grid-add-multiple-rows");
+        btn.off("click")
+        grid.set_multiple_add("item_code", "custom_quantity");
+    },
+
+})
+
+frappe.ui.form.on("Delivery Note Item", {
+    custom_quantity: function (frm, cdt, cdn) {
+        var row = locals[cdt][cdn]
+        frappe.run_serially([
+            () => frappe.timeout(2),
+            () => frappe.model.set_value(cdt, cdn, "qty", row.custom_quantity),
+        ]);
     }
 })
--- delcomp/public/js/purchase_receipt.js
+++ delcomp/public/js/purchase_receipt.js
@@ -14,5 +14,22 @@ frappe.ui.form.on("Purchase Receipt", {
             args: {},
             callback: function (r) {}
         });
+    },
+    onload_post_render: function (frm) {
+        var grid = frm.get_field("items").grid
+        grid.multiple_set = false
+        var btn = $(grid.wrapper).find(".grid-add-multiple-rows");
+        btn.off("click")
+        grid.set_multiple_add("item_code", "custom_quantity");
+    },
+})
+
+frappe.ui.form.on("Purchase Receipt Item", {
+    custom_quantity: function (frm, cdt, cdn) {
+        var row = locals[cdt][cdn]
+        frappe.run_serially([
+            () => frappe.timeout(2),
+            () => frappe.model.set_value(cdt, cdn, "received_qty", row.custom_quantity),
+        ]);
     }
 })
